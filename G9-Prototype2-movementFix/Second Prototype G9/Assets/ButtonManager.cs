f32d958b472787e38169c51244a1fe428043089b
bong <30470346+oreoadidas@users.noreply.github.com>
2018-02-26 20:50:36 +0000

    added ui

---
+++ G9-Prototype2-movementFix/Second Prototype G9/Assets/ButtonManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Cursor.visible = true; //display cursor
+        Cursor.lockState = CursorLockMode.None; //unlocks the cursor
+	}
+
+    IEnumerator DelayPlay()
+    {
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(GetComponent<AudioSource>().clip.length);
+        Application.LoadLevel("Level1");
+    }
+
+    IEnumerator DelayMainMenu()
+    {
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(GetComponent<AudioSource>().clip.length);
+        Application.LoadLevel("MainMenu");
+    }
+
+    public void DelayPlay(string Level1)
+    {
+        StartCoroutine(DelayPlay());
+    }
+
+    public void DelayMenu(string MainMenu)
+    {
+        StartCoroutine(DelayMainMenu());
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
