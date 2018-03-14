72e26aa5368dc981f8a0dfa54285fc135368dca0
bong <30470346+oreoadidas@users.noreply.github.com>
2018-03-14 00:07:02 +0000

    UI Added

---
+++ G9 Game 1.0.6/G9 Game 1.0.5/Assets/Scripts/GcuHealth.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GcuHealth : MonoBehaviour
+{
+    public float health;
+    public float maxHealth = 1f;
+    public int score = 0;
+    public Text scoreText;
+    public Image healthBar;
+    public AudioClip healthLoss;
+    public AudioClip inspiredStudent;
+
+    // Use this for initialization
+    void Start()
+    {
+        health = maxHealth;
+        healthBar.fillAmount = 1;
+        scoreText.text = "Score: " + score.ToString();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            bool lazy = col.GetComponent<EnemyScript>().isLazy;
+            if (lazy)
+            {
+                StartCoroutine(PlaySoundOnHealthLoss());
+                healthBar.fillAmount = health / maxHealth;
+                GcuFail();
+                health -= 10f;
+            }
+
+            bool book = col.GetComponent<EnemyScript>().hasBook;
+            if (book)
+            {
+                healthBar.fillAmount = health / maxHealth;
+                GcuFail();
+                if (health < 100f)
+                {
+                    health += 10f;
+                }
+            }
+
+
+            if (!lazy)
+            {
+                StartCoroutine(PlaySoundOnInspiredStudent());
+                score++;
+                scoreText.text = "Score: " + score.ToString();
+            }
+        }
+    }
+
+    void GcuFail()
+    {
+        if (health <= 0f)
+        {
+            SceneManager.LoadScene("EndScene");
+        }
+    }
+
+    IEnumerator PlaySoundOnHealthLoss()
+    {
+        GetComponent<AudioSource>().PlayOneShot(healthLoss);
+        yield return new WaitForSeconds(
+        GetComponent<AudioSource>().clip.length);
+    }
+
+    IEnumerator PlaySoundOnInspiredStudent()
+    {
+        GetComponent<AudioSource>().PlayOneShot(inspiredStudent);
+        yield return new WaitForSeconds(
+        GetComponent<AudioSource>().clip.length);
+    }
+}
---
+++ G9 Game 1.0.6/G9 Game 1.0.5/Assets/Scripts/Pause.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    public GameObject pauseUI; //pause game UI
+    public bool isPaused; //checks if the game is paused or not
+    public GameObject player;
+
+    // Use this for initialization
+    void Start()
+    {
+        pauseUI.SetActive(false); //game starts out without the UI overlay since it's not paused
+        GetComponent<Movement>().enabled = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            if (Time.timeScale == 1)
+            {
+                isPaused = true;
+                player.GetComponent<Movement>().enabled = false;
+                pauseUI.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                isPaused = false;
+                player.GetComponent<Movement>().enabled = true;
+                pauseUI.SetActive(false);
+                Time.timeScale = 1;
+            }
+    }
+}
